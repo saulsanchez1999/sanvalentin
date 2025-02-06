da7dd0595f4831c4db9036f0d60b9bbbc0668760
saulsanchez1999 <saulgalaxy13@gmail.com>
2025-02-05 19:00:18 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const yesbtn = document.querySelector('#yesbtn');
+
+yesbtn.addEventListener('click',function () {
+    alert('Que felicidad, te amo Chel')
+
+
+
+});
+
+const nobtn = document.querySelector('#nobtn');
+
+nobtn.addEventListener('click',function () {
+    alert('Cortalas Chealsea Romari')
+
+});
